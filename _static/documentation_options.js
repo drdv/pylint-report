7db06709582eb65a559aa1caf6fc10be35be8aec
drdv <mail.mitko@gmail.com>
2022-12-10 11:39:34 +0000

    Deploying to gh-pages from @ drdv/pylint-report@07c66a45af7282eee202874bf2d50db1c3710fb8 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+ge08bf46',
+    VERSION: '0.1.dev1+g07c66a4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
